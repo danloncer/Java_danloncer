794952d22f1d76bcc1badc8988b5d1f3cdf17ce0
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-09-10 21:51:19 +0300

    My Homework for lesson 1.4

--- src/pro/sky/java/course1/lesson4/Homework4.java
+++ src/pro/sky/java/course1/lesson4/Homework4.java
@@ -13,7 +13,7 @@ public class Homework4 {
         task5();
 
         //ЗАДАЧА 5
-        //Боже, сидел 30 минут и перепробывал много способов. Зашел даже в ворд и там сопоставлял переменные
+        //Сидел 30 минут и перепробывал много способов. Зашел даже в ворд и там сопоставлял переменные
         //0  1   1   2   3   5   8   13   21  34
         //n1 n2  n3  n1  n2  n3  n1  n2   n3  n1
         //И ведь я увидел в этом логику, но очень долго думал как написать это в виде кода
@@ -24,18 +24,44 @@ public class Homework4 {
 
     }
 
-    private static void task5() {
-        System.out.println("Задача 5");
+    private static void task1() {
+        System.out.println("Задача 1");
         System.out.print("\n");
-        int num1 = 0;
-        int num2 = 1;
-        int num3;
-        for (int i = 0; i<=3 ; i++) {
-            num3 = num1 + num2;
-            System.out.print(num1 + " " + num2 + " ");
-            System.out.print(num3 + " ");
-            num1 = num2 + num3;
-            num2 = num1 + num3;
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\n");
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+    }
+
+    private static void task2() {
+        System.out.println("Задача 2");
+        System.out.print("\n");
+        int Friday = 6;
+        for (int i = 1; i <= 31; i++) {
+            if (i == Friday) {
+                System.out.println("Сегодня пятница," + i + "-ое число. Необходимо подготовить отчет.");
+                Friday = Friday +7;
+            }
+        }
+        System.out.print("\n");
+    }
+
+    private static void task3() {
+        System.out.println("Задача 3");
+        System.out.print("\n");
+        int year = 2021;
+        int nextYear = year + 100;
+        for ( int lastYear = year - 200; lastYear <= nextYear ; lastYear++) {
+            if (lastYear % 79 == 0) {
+                System.out.println(lastYear);
+            }
         }
         System.out.print("\n");
     }
@@ -61,45 +87,19 @@ public class Homework4 {
         System.out.print("\n");
     }
 
-    private static void task3() {
-        System.out.println("Задача 3");
+    private static void task5() {
+        System.out.println("Задача 5");
         System.out.print("\n");
-        int year = 2021;
-        int nextYear = year + 100;
-        for (int lastYear = year - 200; lastYear <= nextYear; lastYear++) {
-            if (lastYear % 79 == 0) {
-                System.out.println(lastYear);
-            }
+        int num1 = 0;
+        int num2 = 1;
+        int num3;
+        for (int i = 0; i<=3 ; i++) {
+            num3 = num1 + num2;
+            System.out.print(num1 + " " + num2 + " ");
+            System.out.print(num3 + " ");
+            num1 = num2 + num3;
+            num2 = num1 + num3;
         }
         System.out.print("\n");
-    }
-
-    private static void task2() {
-        System.out.println("Задача 2");
-        System.out.print("\n");
-        int Friday = 6;
-        for (int i = 1; i <= 31; i++) {
-            if (i == Friday) {
-                System.out.println("Сегодня пятница," + i + "-ое число. Необходимо подготовить отчет.");
-                Friday = Friday +7;
-            }
-        }
-        System.out.print("\n");
-    }
-
-    private static void task1() {
-        System.out.println("Задача 1");
-        System.out.print("\n");
-        int i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
-        }
-
-        System.out.print("\n");
-        for (i = 10; i > 0; i--) {
-            System.out.print(i + " ");
-        }
-        System.out.println("\n");
     }
 }
