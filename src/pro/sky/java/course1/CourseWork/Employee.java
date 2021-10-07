863b9fbeab762b5427fd0611d196c98652597e0c
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-10-08 01:09:31 +0300

    CourseWork_2.0

---
+++ src/pro/sky/java/course1/CourseWork/Employee.java
@@ -0,0 +1,80 @@
+package pro.sky.java.course1.CourseWork;
+
+
+public class Employee {
+
+
+    //Fields
+
+
+    private final String firstName;
+    private final String middleName;
+    private int department;
+    private float cash;
+    private static int counter = 0;
+    private final int id;
+
+
+
+    //Constructor
+
+    public Employee (String firstName, String middleName, int department, float cash) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        if (department < 5 && department > 0) {
+            this.department = department;
+        } else {
+            throw new RuntimeException("Отдел не должен быть больше 5 и не меньше 0");
+        }
+        this.cash = cash;
+        this.id = Employee.counter;
+        counter++;
+
+    }
+
+
+    //Getters
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public float getCash() {
+        return cash;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    //Setters
+
+
+    public void setCash (float cash) {
+        this.cash = cash;
+    }
+
+    public void setDepartment (int department) {
+        this.department = department;
+    }
+
+
+    //Override
+
+
+    @Override
+    public String toString() {
+        return "Name: " + firstName + " " + middleName + " :Department: " + department + " :Cash: " + cash + " :Id: " +id;
+    }
+
+}
---
+++ src/pro/sky/java/course1/CourseWork/EmployeeBook.java
@@ -0,0 +1,5 @@
+package pro.sky.java.course1.CourseWork;
+
+public class EmployeeBook {
+
+}
